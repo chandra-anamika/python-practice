144e6e7698f692c85832e28b0127c88b75e64158
Anamika <anamikachandra1@gmail.com>
2019-05-13 10:31:15 +0200

    Changes adapted for counting substring from string

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # python-practice
-This is first commit
+Print the number of times that the substring occurs in the given string
+
+Sample Input:
+	Enter the String -  ABCDCDC
+	Enter the Substring - CDC
+
+Sample Output :
+	2
+
+Solution : This has been solved by traversing the String till length of substring from 1st character till the end and increasing the counter if found so.
+
+The length of a string is found by the function len(s), where s is the string. 
+
+
---
+++ findString.py
@@ -0,0 +1,24 @@
+def count_substring(string, sub_string):
+    count=0
+    lenSubstr = len(sub_string)
+    lenStr = len(string)
+    i=0
+
+    #Traversing the string and calculating the number of occurences of substring
+    while i<lenStr:
+        #Matching from ith to length of substring to substring and increasing the counter in case of match 
+        if string[i:i+lenSubstr]==sub_string:
+            count = count +1
+        i=i+1
+    return count
+
+if __name__ == '__main__':
+
+    #Getting input from console
+    print("Enter the String - ",end='')
+    string = input().strip()
+    print("Enter the Substring - ",end='')
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
